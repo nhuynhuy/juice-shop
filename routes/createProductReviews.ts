24f69d3cd156b8dbe1a6b5293524d10f3619cf4c
nhuynhuy1107 <smallmoon2001@gmail.com>
2023-11-28 09:58:30 +0700

    fixed code up review

--- routes/createProductReviews.ts
+++ routes/createProductReviews.ts
@@ -15,11 +15,21 @@ const security = require('../lib/insecurity')
 module.exports = function productReviews () {
   return (req: Request, res: Response) => {
     const user = security.authenticatedUsers.from(req)
+    if (!user) {
+      return res.status(401).json({ status: 'error', message: 'Xác thực là bắt buộc' })
+    }
+
+    // Kiểm tra nội dung review
+    const reviewContent = req.body.message
+    if (!reviewContent || reviewContent.length === 0) {
+      return res.status(400).json({ status: 'error', message: 'Nội dung review không được để trống' })
+    }
+
     challengeUtils.solveIf(challenges.forgedReviewChallenge, () => { return user && user.data.email !== req.body.author })
     reviews.insert({
       product: req.params.id,
       message: req.body.message,
-      author: req.body.author,
+      author: user.data.email, // Sử dụng email của người dùng đã xác thực
       likesCount: 0,
       likedBy: []
     }).then(() => {
@@ -29,3 +39,9 @@ module.exports = function productReviews () {
     })
   }
 }
+
+// Trong đoạn code trên, đã thêm:
+
+// Kiểm tra xác thực người dùng.
+// Kiểm tra nội dung review không được để trống.
+// Sử dụng email của người dùng đã xác thực làm tác giả của review.
